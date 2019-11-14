259eae99b2e4c782bb88ffc3bb14a08ca825efd8
fairb205 <michaelfairbanks5469@gmail.com>
2019-11-13 22:03:47 -0600

    Remove users from client #9
    Remove everything users from all apps, change theming by adding @include

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,14 +4,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {HttpClientModule, HttpClient} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
-import {UserListComponent} from './users/user-list.component';
-import {UserListService} from './users/user-list.service';
+
 import {HomeService} from './home/home.service';
 import {Routing} from './app.routes';
 import {APP_BASE_HREF} from '@angular/common';
 
 import {CustomModule} from './custom.module';
-import {AddUserComponent} from './users/add-user.component';
+
 import {HomeDialog} from './home/home.component';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatSelectModule} from '@angular/material/select';
@@ -43,18 +42,14 @@ import {MatProgressBarModule} from '@angular/material';
   declarations: [
     AppComponent,
     HomeComponent,
-    UserListComponent,
-    AddUserComponent,
     HomeDialog,
   ],
   providers: [
     HttpClient,
-    UserListService,
     HomeService,
     {provide: APP_BASE_HREF, useValue: '/'},
   ],
   entryComponents: [
-    AddUserComponent,
     HomeDialog,
   ],
   bootstrap: [AppComponent]
